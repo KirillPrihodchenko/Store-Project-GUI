e6d8781045d603d5e065fe1db22418e13b571aba
Kyrylo Prykhodchenko <kirprihodchenko@gmail.com>
2023-01-31 10:37:00 +0200

    debigging key parsed

--- src/main/java/StoreLogic/GetPrice.java
+++ src/main/java/StoreLogic/GetPrice.java
@@ -1,50 +1,85 @@
 package StoreLogic;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
 
 public class GetPrice extends Category{
 
-    protected static List<Double> parsedKey = new ArrayList<>();
+    protected static Set<String> keys = new HashSet<>();
 
+    public static Set<String> parsingKeyVegetables(String value) {
 
-    public static List<Double> parsingKeyVegetables(String value) {
+        for (Map.Entry<String, String> entry : vegetables.entrySet()) {
 
-        for(String keys: vegetables.keySet()){
-
-            double keyToDouble = Double.parseDouble(keys);
-            parsedKey.add(keyToDouble);
+            if (Objects.equals(value, entry.getValue())) {
+                keys.add(entry.getKey());
+            }
         }
-        return parsedKey;
+        return keys;
     }
 
-    public static List<Double> parsingKeyMeats() {
+    public static Set<String> parsingKeyMeats(String value) {
 
-        for(String keys: meats.keySet()){
+        for (Map.Entry<String, String> entry : vegetables.entrySet()) {
 
-            double keyToDouble = Double.parseDouble(keys);
-            parsedKey.add(keyToDouble);
+            if (Objects.equals(value, entry.getValue())) {
+                keys.add(entry.getKey());
+            }
         }
-        return parsedKey;
+        return keys;
     }
 
-    public static List<Double> parsingKeyCheeses() {
+    public static Set<String> parsingKeyCheeses(String value) {
 
-        for(String keys: cheeses.keySet()){
+        for (Map.Entry<String, String> entry : vegetables.entrySet()) {
 
-            double keyToDouble = Double.parseDouble(keys);
-            parsedKey.add(keyToDouble);
+            if (Objects.equals(value, entry.getValue())) {
+                keys.add(entry.getKey());
+            }
         }
-        return parsedKey;
+        return keys;
     }
 
-    public static List<Double> parsingKeyFish() {
+    public static Set<String> parsingKeyFish(String value) {
 
-        for(String keys: fish.keySet()){
+        for (Map.Entry<String, String> entry : vegetables.entrySet()) {
 
-            double keyToDouble = Double.parseDouble(keys);
-            parsedKey.add(keyToDouble);
+            if (Objects.equals(value, entry.getValue())) {
+                keys.add(entry.getKey());
+            }
         }
-        return parsedKey;
+        return keys;
     }
+
+   /** overriding coming soon*/
+//    @Override
+//    public int hashCode() {
+//        final int prime = 31;
+//        int result = 1;
+//        result = prime * result + ((vegetables.isEmpty()) ? 0 : vegetables.hashCode());
+//        result = prime * result + ((meats.isEmpty()) ? 0 : meats.hashCode());
+//        result = prime * result + ((cheeses.isEmpty()) ? 0 : cheeses.hashCode());
+//        result = prime * result + ((fish.isEmpty()) ? 0 : fish.hashCode());
+//
+//        return result;
+//    }
+//
+//    @Override
+//    public boolean equals(Object obj) {
+//
+//        if (obj == this) {
+//            return true;
+//        }
+//        if (obj == null || obj.getClass() != this.getClass()) {
+//            return false;
+//        }
+//
+//        Category category = (Category) obj;
+//        return ((!vegetables.isEmpty()) &&
+//                (!meats.isEmpty()) &&
+//                (!cheeses.isEmpty()) &&
+//                (!fish.isEmpty()));
+//    }
 }
